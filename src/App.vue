24b95d593aeaa2caa463b74b5cb0658685010f3c
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-06 21:10:43 +1000

    finished

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Vue app</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  template: `<div>adf</div>`
+})
+</script>
