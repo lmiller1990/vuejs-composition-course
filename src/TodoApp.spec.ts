30790a62731e67a6533b60cd2783b7c590112b20
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-07 22:55:34 +1000

    add js example

---
+++ src/TodoApp.spec.ts
@@ -0,0 +1,22 @@
+import { mount } from '@lmiller1990/vue-test-utils-next'
+
+import TodoApp from './TodoApp.vue'
+
+test('renders a todo', () => {
+  const wrapper = mount(TodoApp)
+
+  const todo = wrapper.find('[data-test="todo"]')
+
+  expect(todo.text()).toBe('Learn Vue.js 3')
+})
+
+test('creates a todo', async () => {
+  const wrapper = mount(TodoApp)
+  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(1)
+
+  wrapper.find<HTMLInputElement>('[data-test="new-todo"]').element.value = 'New todo'
+  await wrapper.find('[data-test="form"]').trigger('submit')
+
+  expect(wrapper.findAll('[data-test="todo"]')).toHaveLength(2)
+})
+
