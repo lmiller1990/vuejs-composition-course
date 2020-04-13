24f248918b5a117323a52c22f0ba285e3ea2f2de
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-13 23:54:59 +1000

    1.6 filter post with computed

--- src/Timeline.spec.ts
+++ src/Timeline.spec.ts
@@ -28,9 +28,19 @@ describe('Timeline', () => {
     expect($thisMonth.classes()).toContain('is-active')
   })
 
-  it('renders todays post by default', () => {
+  it('renders todays post by default', async () => {
     const wrapper = mount(Timeline)
     
     expect(wrapper.findAll('[data-test="post"]')).toHaveLength(1)
+
+    const $thisWeek = wrapper.findAll('[data-test="period"]')[1]
+    await $thisWeek.trigger('click')
+
+    expect(wrapper.findAll('[data-test="post"]')).toHaveLength(2)
+
+    const $thisMonth = wrapper.findAll('[data-test="period"]')[2]
+    await $thisMonth.trigger('click')
+
+    expect(wrapper.findAll('[data-test="post"]')).toHaveLength(3)
   })
 })
--- src/Timeline.vue
+++ src/Timeline.vue
@@ -8,18 +8,53 @@
         {{ period }}
       </a>
     </p>
+
+    <a data-test="post" class="panel-block" v-for="post in posts" :key="post.id">
+      <div>
+        <a>{{post.title}}</a>
+        <div>{{ post.created.format('Do MMM') }}</div>
+      </div>
+    </a>
   </nav>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, computed } from 'vue'
+import moment from 'moment'
 
-import { Period } from './types'
+import { Period, Post } from './types'
+import { todayPost, thisWeek, thisMonth } from './mocks'
 
 export default defineComponent({
   setup() {
     const periods: Period[] = ['today', 'this week', 'this month']
     const selectedPeriod = ref<Period>('today')
+
+    const posts = computed(() => [todayPost, thisWeek, thisMonth].filter(post => {
+        if (
+          selectedPeriod.value === 'today' &&
+          post.created.isAfter(moment().subtract(1, 'day'))
+        ) {
+          return true
+        }
+
+        if (
+          selectedPeriod.value === 'this week' &&
+          post.created.isAfter(moment().subtract(7, 'day'))
+        ) {
+          return true
+        }
+
+        if (
+          selectedPeriod.value === 'this month' &&
+          post.created.isAfter(moment().subtract(1, 'month'))
+        ) {
+          return true
+        }
+
+        return false
+      })
+    )
 
     const setPeriod = (period: Period) => {
       selectedPeriod.value = period
@@ -28,7 +63,8 @@ export default defineComponent({
     return {
       periods,
       selectedPeriod,
-      setPeriod
+      setPeriod,
+      posts
     }
   }
 })
