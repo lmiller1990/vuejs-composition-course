d75c2ae797cdf68b38da041f33a944b7c7aab640
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-04-29 23:11:13 +1000

    add store

---
+++ src/store.ts
@@ -0,0 +1,37 @@
+import { reactive, readonly } from "vue"
+import { Post } from "./types"
+
+interface PostsState {
+  ids: string[]
+  all: Record<string, Post>
+  loaded: boolean
+}
+
+interface State {
+  posts: PostsState
+}
+
+const initialPostsState = (): PostsState => ({
+  all: {},
+  ids: [],
+  loaded: false
+})
+
+const initialState = (): State => ({
+  posts: initialPostsState()
+})
+
+class Store {
+  protected state: State
+
+  constructor(initialState: State) {
+    this.state = reactive(initialState)
+  }
+
+  public getState(): State {
+    return readonly(this.state)
+  }
+}
+
+const store = new Store(initialState())
+store.getState()
