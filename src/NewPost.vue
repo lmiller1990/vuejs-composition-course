dfbd2d3f97a5cf1971415c9aebafdd89b22dabdf
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-04 22:28:42 +1000

    add a post writer

--- src/NewPost.vue
+++ src/NewPost.vue
@@ -1,11 +1,33 @@
 <template>
-  <div>New Post</div>
+  <PostWriter :post="post" />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import PostWriter from './PostWriter.vue'
+import { Post } from './types'
+import moment=require('moment')
 
 export default defineComponent({
   name: 'NewPost',
+
+  components: {
+    PostWriter
+  },
+
+  setup() {
+    const post: Post = {
+      id: -1,
+      title: 'New Post',
+      markdown: '## New Post\nEnter your post here...',
+      html: '',
+      created: moment(),
+      authorId: 0
+    }
+
+    return {
+      post
+    }
+  }
 })
 </script>
---
+++ src/PostWriter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column">
+        <div class="field">
+          <div class="label">Post Title</div>
+          <div class="control">
+            <input v-model="title" type="text" class="input" />
+            {{ title }}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { Post } from './types'
+
+export default defineComponent({
+  name: 'PostWriter',
+
+  props: {
+    post: {
+      type: Object as () => Post,
+      required: true
+    }
+  },
+
+  setup(props) {
+    const title = ref(props.post.title)
+
+    return {
+      title
+    }
+  }
+})
+</script>
