dcf8d10a09be0a1d459edfc30e22b1060b4780df
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-05-04 23:15:22 +1000

    use a template ref

--- src/PostWriter.vue
+++ src/PostWriter.vue
@@ -11,11 +11,18 @@
         </div>
       </div>
     </div>
+
+    <div class="columns">
+      <div class="column is-one-half">
+        <div contenteditable id="markdown" ref="contentEditable" />
+      </div>
+      <div class="column is-one-half"></div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import { Post } from './types'
 
 export default defineComponent({
@@ -30,9 +37,18 @@ export default defineComponent({
 
   setup(props) {
     const title = ref(props.post.title)
+    const contentEditable = ref<null | HTMLDivElement>(null)
+
+    console.log(contentEditable.value)
+
+    onMounted(() => {
+      console.log(contentEditable.value)
+      // ...
+    })
 
     return {
-      title
+      title,
+      contentEditable
     }
   }
 })
