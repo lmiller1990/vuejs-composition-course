5ba88f5a3f5ef50b68e1da836d8c1a0c71232023
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-15 12:07:22 +1000

    update

--- update.sh
+++ update.sh
@@ -6,7 +6,7 @@ for branch in $branches
 do
   git checkout $branch
   echo "-- Updating $branch --"
-  git checkout 6.4-test-stubs-components -- index.d.ts index.html yarn.lock package.json webpack.config.js jest.config.js tsconfig.json
+  git checkout master -- index.d.ts index.html yarn.lock package.json webpack.config.js jest.config.js tsconfig.json
   git commit -m "update config files"
   break
   git push --set-upstream vuejs-composition-course $branch
