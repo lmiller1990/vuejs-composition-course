0ca142783ea4800992227360efa6dd98d3653b32
Lachlan Miller <lachlan.miller.1990@outlook.com>
2020-08-15 12:10:41 +1000

    work on script

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ branches="$(git branch | grep  '1\.')"
 arr1=(1.0-getting-started 1.1-creating-timeline-component 1.2-our-first-test 1.3-reactive-updates-using-ref 1.4-testing-active-tab 1.5-getting-started-with-tdd 1.6-filtering-posts-with-computed 1.7-refactoring-with-confidence
 )
 
-for branch in ${arr[@]}
+for branch in ${arr1[@]}
 do
   git checkout $branch
   echo "-- Updating $branch --"
